6e0907eaf34aef803b8b46fb02682df3f865fc46
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-24 12:28:17 -0300

    feat: classe Turma(getters e função de exibir_info())

--- turma.py
+++ turma.py
@@ -1,1 +1,32 @@
 # Classe Turma
+from curso import Curso
+
+class Turma:
+    def __init__(self, codigo: str, curso: Curso, alunos: list = None):
+        if alunos is None:
+            alunos = []
+        self.__codigo = codigo
+        self.__curso = curso
+        self.__alunos = alunos
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @property
+    def curso(self):
+        return self.__curso
+
+    @property
+    def alunos(self):
+        return self.__alunos        
+    
+    def exibir_info(self):
+        print(f"Código: {self.codigo}\n Curso: {self.curso.nome}\n Alunos: ")
+        if (self.alunos):
+            for a in self.alunos:
+                print(f" - {a.nome}")
+        else:
+            print("Não há alunos cadastrados!")
+
+    
