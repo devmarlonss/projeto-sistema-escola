f037bb41cf484623c237816bc1029c6cad588571
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 15:16:25 -0300

    feat: classe Sistema

---
+++ sistema.py
@@ -0,0 +1,8 @@
+class Sistema:
+    def __init__(self):
+        self.__disciplinas = []
+        self.__cursos = []
+        self.__turmas = []
+        self.__alunos = []
+        self.__professores = []
+        self.__adms = []
