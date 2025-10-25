e0b81586bd8186173d0e387635fcb2568f5095a4
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 11:22:31 -0300

    feat: classe Professor(getters e função de exibir_info())

--- professor.py
+++ professor.py
@@ -1,1 +1,22 @@
 # Classe Professor
+from usuario import Usuario
+from disciplina import Disciplina
+
+class Professor(Usuario):
+    def __init__(self, nome, cpf, email, senha, disciplina: Disciplina, salario: float):
+        super().__init__(nome, cpf, email, senha)
+        self.__disciplina = disciplina
+        self.__salario = salario
+
+    @property 
+    def disciplina(self):
+        return self.__disciplina
+    
+    @property
+    def salario(self):
+        return self.__salario
+    
+    def exibir_info(self):
+        super().exibir_info()
+        print(f"Disciplina: {self.disciplina.nome}\nSalário: {self.salario:.2f}")
+    
