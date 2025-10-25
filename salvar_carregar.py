1665bc6e9cfd77997f21cd253e46ef66a2c0c032
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 14:10:22 -0300

    feat: funções para salvar/carregar dados json

--- salvar_carregar.py
+++ salvar_carregar.py
@@ -1,1 +1,13 @@
 # Funções para salvar/carregar JSON
+import json
+
+def salvar_dados(arquivo, dados):
+    with open(("Dados/" + arquivo), "w", encoding="utf-8") as arq:
+        json.dump(dados, arq, indent=4, ensure_ascii=False)
+
+def carregar_dados(arquivo, dados):
+    try:
+        with open(("Dados/" + arquivo), "r", encoding="utf-8") as arq:
+            return json.load(arq)
+    except FileNotFoundError:
+        return {}
