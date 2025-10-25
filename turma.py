16b986d60cb2d8e9f0e8e6de15ea16af86247851
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 11:07:59 -0300

    test: verificar adição e remoção de aluno à turma

--- turma.py
+++ turma.py
@@ -44,6 +44,15 @@ class Turma:
 
 if __name__ == "__main__":
     curso1 = Curso("Informática", "C001")
+    aluno1 = Aluno("Marlon", "12312312312", "example@gmail.com", "senha123", curso1)
+    aluno2 = Aluno("Rian", "12312311211", "example@gmail.com", "senha123", curso1)
 
     turma1 = Turma("T001", curso1)
+    turma1.exibir_info()
+    print()
+    turma1.adicionar_aluno(aluno1)
+    turma1.adicionar_aluno(aluno2)
+    turma1.exibir_info()
+    print()
+    turma1.remover_aluno(aluno2)
     turma1.exibir_info()
