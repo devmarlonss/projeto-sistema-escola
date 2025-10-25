28638241040bd3f1056d27cefcb82bacbac364df
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 14:04:18 -0300

    feat: funções de de/serialização

--- disciplina.py
+++ disciplina.py
@@ -21,6 +21,17 @@ class Disciplina:
     def exibir_info(self):
         print(f"Nome: {self.nome}\nCódigo: {self.codigo}\nCarga Horária: {self.carga_horaria} horas")
 
+    def disc_dict(self):
+        return {
+            "nome": self.nome,
+            "codigo": self.codigo,
+            "carga_horaria": self.carga_horaria
+        }
+    
+    @staticmethod
+    def dict_disc(disc):
+        return Disciplina(disc["nome"], disc["codigo"], disc["carga_horaria"])
+
 if __name__ == "__main__":
     disc = Disciplina("Matemática", "D001", 120)
     disc.exibir_info()
