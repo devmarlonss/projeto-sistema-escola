c423ede81baa57ca9e69c6fa92f65238f925cc95
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 11:32:21 -0300

    feat: lançar notas de alunos

--- professor.py
+++ professor.py
@@ -1,6 +1,7 @@
 # Classe Professor
 from usuario import Usuario
 from disciplina import Disciplina
+from aluno import Aluno
 
 class Professor(Usuario):
     def __init__(self, nome, cpf, email, senha, disciplina: Disciplina, salario: float):
@@ -19,6 +20,13 @@ class Professor(Usuario):
     def exibir_info(self):
         super().exibir_info()
         print(f"Disciplina: {self.disciplina.nome}\nSalário: R$ {self.salario:.2f}")
+
+    def lancar_notas(self, aluno, disciplina, nota):
+        if (isinstance(aluno, Aluno)):
+            if (aluno.adicionar_nota(disciplina, nota)):
+                return True
+            return False
+        return False    
     
 if __name__ == "__main__":
     disc = Disciplina("Matemática", "D001", 120)
