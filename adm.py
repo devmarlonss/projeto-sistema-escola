5c796d89979eb38852e5bf043520c14f76914c87
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-25 12:33:01 -0300

    feat: classe Adm com os métodos para adicionar novos usuarios

--- adm.py
+++ adm.py
@@ -1,1 +1,22 @@
 # Classe Adm
+from usuario import Usuario
+from aluno import Aluno
+from professor import Professor
+from disciplina import Disciplina
+from curso import Curso
+from turma import Turma
+
+class Adm(Usuario):
+    def __init__(self, nome, cpf, email, senha):
+        super().__init__(nome, cpf, email, senha)
+
+    # Gerência de Usuários
+    def add_aluno(self, nome, cpf, email, senha, curso):
+        return Aluno(nome, cpf, email, senha, curso)
+    
+    def add_prof(self, nome, cpf, email, senha, disciplina, salario):
+        return Professor(nome, cpf, email, senha, disciplina, salario)
+    
+    @classmethod
+    def add_adm(cls, nome, cpf, email, senha):
+        return cls(nome, cpf, email, senha)
