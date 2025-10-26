119f8762bbb26fbb9d795b9a01f7391cc12c3444
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-26 11:29:36 -0300

    feat: de/serialização de objetos/dicts adm

--- adm.py
+++ adm.py
@@ -5,4 +5,9 @@ class Adm(Usuario):
     def __init__(self, nome, cpf, email, senha):
         super().__init__(nome, cpf, email, senha)
 
+    def adm_dict(self):
+        return super().usuario_dict()
     
+    @staticmethod
+    def dict_adm(adm):
+        return Adm(adm["nome"], adm["cpf"], adm["email"], adm["senha"])
