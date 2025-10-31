6aa6499d2073597bb4c313da689765de62eae6c0
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-10-31 12:52:48 -0300

    docs: adicionar docstrings aos métodos da classe Adm (adm_dict, dict_adm)

--- adm.py
+++ adm.py
@@ -6,10 +6,12 @@ class Adm(Usuario):
         super().__init__(nome, cpf, email, senha)
 
     def adm_dict(self):
+        """Retorna o objeto da classe Adm em formato de dicionário."""
         dados = super().usuario_dict()
         dados["tipo"] = "Adm"
         return dados
     
     @staticmethod
     def dict_adm(adm):
+        """Cria uma instância da classe Adm a partir de um dicionário"""
         return Adm(adm["nome"], adm["cpf"], adm["email"], adm["senha"])
